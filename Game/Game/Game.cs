8f2d9017857e4331d85e3ad35a4e4b5c985fad8f
adam flax <adam.flax@huddle.net>
2012-08-29 14:40:51 +0100

    cleaned up Game.cs removing old code and no shit sherlock style comments

--- Game/Game/Game.cs
+++ Game/Game/Game.cs
@@ -15,7 +15,6 @@ namespace Game
     {
         readonly GraphicsDeviceManager _graphics;
         SpriteBatch _spriteBatch;
-        private  InputManager _input;
         private ScreenManager _screenmanager;
 
         public Game()
@@ -25,59 +24,30 @@ namespace Game
             
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            _graphics.PreferredBackBufferWidth = (int)GameData.ScreenSize.X;
-            _graphics.PreferredBackBufferHeight = (int)GameData.ScreenSize.Y;
+            CreateGameSettings();
             _screenmanager = ScreenManager.Instance(Content);
-            IsMouseVisible = true;
 
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             _spriteBatch = new SpriteBatch(GraphicsDevice);
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// all content.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
+            Content.Unload();
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            //_input.Update(Keyboard.GetState());
-            //_player.Update(gameTime);
             _screenmanager.GetScreen().Update(gameTime);
-
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
@@ -87,6 +57,13 @@ namespace Game
             _spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        private void CreateGameSettings()
+        {
+            _graphics.PreferredBackBufferWidth = (int)GameData.ScreenSize.X;
+            _graphics.PreferredBackBufferHeight = (int)GameData.ScreenSize.Y;
+            IsMouseVisible = true;
         }
     }
 }
